9e8e515572afbc5bdaf5a4b61ac246a25073e310
Malaika Tauro <64835666+malaikabello@users.noreply.github.com>
2020-07-05 14:47:32 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,8 +1,6 @@
->  data <- read.table("C:/Users/malai/Downloads/exdata_data_household_power_consumption (1)/household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
->  summary(data)
-
-> subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
-> 
+> data <- read.table("C:/Users/malai/Downloads/exdata_data_household_power_consumption (1)/household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+> summary(data)
+> subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),] 
 > globalActivePower <- as.numeric(subsetdata$Global_active_power)
 > globalReactivePower <- as.numeric(subsetdata$Global_reactive_power)
 > voltage <- as.numeric(subsetdata$Voltage)
